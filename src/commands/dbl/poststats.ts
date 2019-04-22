bb74a1fb646787f9f69e257e7f7ceda346b27dd3
Federico Grandi <fgrandi30@gmail.com>
2019-04-22 12:26:37 +0200

    Add manual DBL stats posting

---
+++ src/commands/dbl/poststats.ts
@@ -0,0 +1,29 @@
+import * as Commando from 'discord.js-commando';
+
+import { post } from '../../utils/dbl_stats';
+
+export default class PostStatsCMD extends Commando.Command {
+  constructor(client: Commando.CommandoClient) {
+    super(client, {
+      name: 'poststats',
+      aliases: ['postdblstats'],
+      group: 'dbl',
+      memberName: 'poststats',
+      description: 'Manually posts bot stats to Discord Bots List\'s API',
+      guildOnly: false,
+      ownerOnly: true
+    });
+  }
+
+  //@ts-ignore
+  async run(msg: Commando.CommandoMessage) {
+    var rep = await msg.say('Posting stats...');
+    if (rep instanceof Array) rep = rep[0];
+
+    try {
+      await post();
+    } catch (e) {
+      rep.edit('Couldn\'t post stats:\n```\n' + e + '\n```');
+    }
+  }
+}
