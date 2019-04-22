0d511121869305354cc6eb54f391f2752122ce72
Federico Grandi <fgrandi30@gmail.com>
2019-04-22 16:00:16 +0200

    Add devstats

---
+++ src/commands/dev/devstats.ts
@@ -0,0 +1,30 @@
+import * as Commando from 'discord.js-commando';
+
+import { client } from '../../core/app';
+
+export default class SampleCMD extends Commando.Command {
+  constructor(client: Commando.CommandoClient) {
+    super(client, {
+      name: 'devstats',
+      group: 'dev',
+      memberName: 'devstats',
+      description: 'Gives some usefult stats about the bot.',
+      args: [{
+        key: 'mode',
+        prompt: 'One of: servers/guilds, database/db',
+        type: 'string',
+        oneOf: ['servers', 'guilds', 'database', 'db']
+      }],
+      guildOnly: false,
+      ownerOnly: true
+    });
+  }
+
+  //@ts-ignore
+  run(msg: Commando.CommandoMessage, { mode }) {
+    if (['servers', 'guilds'].includes(mode)) {
+      var text = `The bot is now in ${client.guilds.size} guilds:\n` + '```\n' + client.guilds.array().join(', ') + '\n```';
+      msg.say(text);
+    }
+  }
+}
