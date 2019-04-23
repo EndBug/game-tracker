41645b9122d9985a8692997f9622005c98d836af
Federico Grandi <fgrandi30@gmail.com>
2019-04-23 19:53:50 +0200

    Devstats - formatting improvent

--- src/commands/dev/devstats.ts
+++ src/commands/dev/devstats.ts
@@ -28,11 +28,9 @@ export default class DevStatsCMD extends Commando.Command {
     } else if (['database', 'db'].includes(mode)) {
       //@ts-ignore
       const { ow }: { ow: Object } = client.provider.settings.get('global');
-      msg.say(`Here is the current database:
-      - Overwatch (${Object.keys(ow).length}):
-      \`\`\`json
-      ${JSON.stringify(ow)}
-      \`\`\``);
+      msg.say('Here is the current database:\n' +
+        `- Overwatch (${Object.keys(ow).length}):\n` +
+        '```json\n' + JSON.stringify(ow) + '\n```');
     }
   }
 }
