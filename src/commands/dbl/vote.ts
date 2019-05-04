29306331390c871581bbf2191856e603f9633746
Federico Grandi <fgrandi30@gmail.com>
2019-05-04 14:47:05 +0200

    Add vote command

---
+++ src/commands/dbl/vote.ts
@@ -0,0 +1,30 @@
+import * as Commando from 'discord.js-commando';
+import { RichEmbed } from 'discord.js';
+import { getVotes } from '../../utils/dbl_stats';
+
+export default class VoteCMD extends Commando.Command {
+  constructor(client: Commando.CommandoClient) {
+    super(client, {
+      name: 'vote',
+      group: 'dbl',
+      memberName: 'vote',
+      description: 'Gives you instructions on how to vote for the bot.',
+      guildOnly: false,
+      ownerOnly: false
+    });
+  }
+
+  //@ts-ignore
+  async run(msg: Commando.CommandoMessage) {
+    const embed = new RichEmbed()
+      .addField('Voting', 'This bot is listed in [Discord Bots List](https://discordbots.org), a popular website that shows nearly every public bot for Discord servers. In order to get more views and reach more people the bot needs to get votes (they get reset monthly).')
+      .addField('Hot to vote', 'Head to the [vote page](https://discordbots.org/bot/475421235950518292/vote) and click the \'Vote\' button; if you\'re not already logged in with Discord it\'ll ask you that first.')
+      .addField('Reminders', 'If you really care about the bot you can also set reminders by clicking the \'Yes, remind me\' button: that will send notifications to your browser evey time you\'re able to vote again (12 hours after one vote). If you prefer, you can also get daily notifications by checking the \'Remind me daily\' box.')
+      .setTimestamp();
+
+    const votes = await getVotes();
+    if (votes instanceof Array) embed.addField('Current votes', votes.length);
+
+    msg.say({ embed });
+  }
+}
--- src/utils/dbl_stats.ts
+++ src/utils/dbl_stats.ts
@@ -29,3 +29,19 @@ export async function post(log = true) {
  * The interval that periodically post bot stats to Discord Bots List's API
  */
 export var postStatsInterval = available ? setInterval(post, 1800000) : undefined;
+
+/**
+ * Gets the current votes for the bot
+ */
+export async function getVotes() {
+  if (!available) throw new Error('There is no loaded DBL client: please check that you\'ve added the DBL_TOKEN to your .env file.');
+
+  try {
+    const votes = await dbl.getVotes();
+    return votes;
+  } catch (e) {
+    const msg = `Can't post bot stats:\n${e}`;
+    if (owner) owner.send(msg);
+    else throw new Error('(Owner not available)\n' + msg);
+  }
+}
