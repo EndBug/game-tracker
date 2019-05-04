b8f942b6d6c36e19b7ec8d4edc783300e8472aef
Federico Grandi <fgrandi30@gmail.com>
2019-05-04 16:27:49 +0200

    Fix typo

--- src/commands/dbl/vote.ts
+++ src/commands/dbl/vote.ts
@@ -18,7 +18,7 @@ export default class VoteCMD extends Commando.Command {
   async run(msg: Commando.CommandoMessage) {
     const embed = new RichEmbed()
       .addField('Voting', 'This bot is listed in [Discord Bots List](https://discordbots.org), a popular website that shows nearly every public bot for Discord servers. In order to get more views and reach more people the bot needs to get votes (they get reset monthly).')
-      .addField('Hot to vote', 'Head to the [vote page](https://discordbots.org/bot/475421235950518292/vote) and click the \'Vote\' button; if you\'re not already logged in with Discord it\'ll ask you that first.')
+      .addField('How to vote', 'Head to the [vote page](https://discordbots.org/bot/475421235950518292/vote) and click the \'Vote\' button; if you\'re not already logged in with Discord it\'ll ask you that first.')
       .addField('Reminders', 'If you really care about the bot you can also set reminders by clicking the \'Yes, remind me\' button: that will send notifications to your browser evey time you\'re able to vote again (12 hours after one vote). If you prefer, you can also get daily notifications by checking the \'Remind me daily\' box.')
       .setTimestamp();
 
