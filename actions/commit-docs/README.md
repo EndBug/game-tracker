7853a8d920214540764dc511f664507f12ff9a60
Federico Grandi <fgrandi30@gmail.com>
2019-09-06 21:20:49 +0200

    Add GitHub Actions (#18)
    - Add automatic doc builds

---
+++ actions/commit-docs/README.md
@@ -0,0 +1,3 @@
+# commit-docs
+
+This action was forked from [stefanzweifel/git-auto-commit-action](https://github.com/stefanzweifel/git-auto-commit-action) and adapted to suite Game Tracker's needs. Click [here](https://github.com/stefanzweifel/git-auto-commit-action/blob/master/LICENSE) to check the original license.
---
+++ actions/commit-docs/entrypoint.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+set -eu
+
+# Set up .netrc file with GitHub credentials
+git_setup ( ) {
+  cat <<- EOF > $HOME/.netrc
+        machine github.com
+        login $GITHUB_ACTOR
+        password $GITHUB_TOKEN
+
+        machine api.github.com
+        login $GITHUB_ACTOR
+        password $GITHUB_TOKEN
+EOF
+    chmod 600 $HOME/.netrc
+
+    git config --global user.email "actions@github.com"
+    git config --global user.name "Game Tracker Actions"
+}
+
+
+# This section only runs if there have been file changes
+echo "Checking for uncommitted changes in the git working tree..."
+if ! git diff --cached --exit-code
+then
+    git_setup
+
+    git fetch 
+
+    # Switch to branch from current Workflow run
+    echo "Creating and switching branch..."
+    git branch "${GITHUB_REF:11}"
+    git checkout "${GITHUB_REF:11}"
+
+    echo "Adding files..."
+    git add doc
+
+    echo "Creating commit..."
+    git commit -m "$INPUT_COMMIT_MESSAGE" --author="$INPUT_COMMIT_AUTHOR_NAME <$INPUT_COMMIT_AUTHOR_EMAIL>"
+
+    echo "Pushing to repo..."
+    git push --set-upstream origin "${GITHUB_REF:11}"
+else
+    echo "Working tree clean. Nothing to commit."
+fi
