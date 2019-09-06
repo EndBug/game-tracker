15f00ab6c4d2920cb38c9026b3c347153fc2f255
Federico Grandi <fgrandi30@gmail.com>
2019-09-06 20:36:28 +0200

    Switch to local action

---
+++ actions/commit-docs/entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -eu
+
+# Set up .netrc file with GitHub credentials
+git_setup ( ) {
+  cat <<- EOF > $HOME/.netrc
+        machine github.com
+        login $GITHUB_ACTOR
+        password $GITHUB_TOKEN
+
+        machine api.github.com
+        login $GITHUB_ACTOR
+        password $GITHUB_TOKEN
+EOF
+    chmod 600 $HOME/.netrc
+
+    git config --global user.email "actions@github.com"
+    git config --global user.name "GitHub Actions"
+}
+
+
+# This section only runs if there have been file changes
+echo "Checking for uncommitted changes in the git working tree."
+if ! git diff --cached --exit-code
+then
+    git_setup
+
+    # Switch to branch from current Workflow run
+    git checkout "${GITHUB_REF:11}"
+
+    git add doc
+
+    git commit -m "$INPUT_COMMIT_MESSAGE" --author="$INPUT_COMMIT_AUTHOR_NAME <$INPUT_COMMIT_AUTHOR_EMAIL>"
+
+    git push --set-upstream origin "${GITHUB_REF:11}"
+else
+    echo "Working tree clean. Nothing to commit."
+fi
