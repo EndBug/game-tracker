08786dafb74802b14a8557e0c682eacb303b1711
Federico Grandi <fgrandi30@gmail.com>
2019-10-23 22:19:53 +0200

    User count in status rotation & 'devstats' fix (#43)
    * Add userCount status
    
    * Fix devstats
    
    * Fix status rotation
    
    * Sort and trim guild count
    
    * Fix rotation
    
    * Fix rotation
    
    * Fix typo
    
    * Invert sort order

--- src/automation/status_rotation.ts
+++ src/automation/status_rotation.ts
@@ -3,9 +3,7 @@ import { client } from '../core/app';
 
 const interval = 12500;
 
-/**
- * Class that makes it easier to add custom statuses (add them into the `status` array)
- */
+/** Class that makes it easier to add custom statuses (add them into the `status` array) */
 class Presence {
   status: PresenceStatus
   afk: boolean
@@ -27,25 +25,27 @@ class Presence {
 
   /** Replaces the dynamic variables inside the presence name */
   getName() {
-    return this.game.name.replace(new RegExp('/guildCount/', 'g'), client.guilds.size.toString());
+    return this.game.name
+      .replace(new RegExp('/guildCount/', 'g'), client.guilds.size.toString())
+      .replace(new RegExp('/userCount/', 'g'), client.users.size.toString());
   }
 }
 
 const status = [
   new Presence('for your requests!', 'WATCHING'),
-  new Presence('/guildCount/ servers.', 'WATCHING')
+  new Presence('/guildCount/ servers.', 'WATCHING'),
+  new Presence('/userCount/ users.', 'LISTENING')
 ];
 
 var index = 0;
 
-/**
- * Reads a custom `Presence` instance and sets it to the client user
+/** Reads a custom `Presence` instance and sets it to the client user
  * @param pres The `Presence` to read
  */
-function setPresence(pres: Presence) {
+function setPresence(indexOverride?: number) {
+  const pres = indexOverride ? status[indexOverride] : status[index];
   index++;
   index %= status.length;
-
   client.user.setStatus(pres.status);
   client.user.setActivity(pres.getName(), {
     type: pres.game.type
@@ -53,5 +53,5 @@ function setPresence(pres: Presence) {
 }
 
 
-if (status.length > 0) setPresence(status[0]);
-if (status.length > 1) setInterval(setPresence, interval, status[index]);
+if (status.length > 0) setPresence();
+if (status.length > 1) setInterval(setPresence, interval);
