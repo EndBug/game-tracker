78359892051aea5403c3de12537f8c60b726f8e8
Federico Grandi <fgrandi30@gmail.com>
2019-11-09 18:16:19 +0100

    FInish examples

--- src/commands/r6/general.ts
+++ src/commands/r6/general.ts
@@ -7,7 +7,10 @@ export default class R6GeneralWiki extends Command {
       description: 'Displays general stats for the given play types.',
       details: 'Specify the play type to show by writing either `pvp`, `pve` or `all`.',
       examples: {
-        'all Snake_Nade': 'Displays general PvP and PvE stats fpr `Snake_Nade` by searching the user in the `uplay` category.'
+        'all Snake_Nade': 'Displays general PvP and PvE stats for `Snake_Nade` by searching the user in the `uplay` category.',
+        'pvp Saunshi xbl': 'Displays general PvP stats for `Saunshi` by searching the user in the `xbl` category.',
+        'all @YoMama': 'Displays general PvP and PvE stats for the mentioned user, if they linked their account.',
+        'all': 'Displays your general PvP and PvE stats, if you linked your account.'
       },
       extra: 'playType'
     }));
