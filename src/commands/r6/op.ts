eb56e3b3324a81aff98452d3a660cd3ad51e0528
Federico Grandi <fgrandi30@gmail.com>
2020-01-01 20:02:57 +0100

    Add link to online docs

--- src/commands/r6/op.ts
+++ src/commands/r6/op.ts
@@ -1,12 +1,11 @@
 import { Command, CommandoClient } from 'discord.js-commando' // eslint-disable-line no-unused-vars
 import { getConfig } from './r6'
-import { constants } from '../../apis/rainbow'
 
 export default class R6OPWiki extends Command {
   constructor(client: CommandoClient) {
     super(client, getConfig('op', {
       description: 'Displays operator stats for the given operator.',
-      details: `Specify the operator to show by writing their name. Every operator can be written in lowercase with no spaces or dots, e.g.: "Recruit SAS" = "recruitsas".\nSupported operator names are: ${constants.OPERATORS.map(op => '`' + op.name + '`').join(', ')}.`,
+      details: 'Specify the operator to show by writing their name. Every operator can be written in lowercase with no spaces or dots, e.g.: "Recruit SAS" = "recruitsas".\nYou can find codes for supported operators here: <https://github.com/EndBug/game-tracker/blob/master/doc/r6/R6_NAMES.md#operators>',
       examples: {
         'recruitsas Snake_Nade': 'Displays Recruit SAS stats for `Snake_Nade` by searching the user in the `uplay` category.',
         'gridlock Saunshi xbl': 'Displays Gridlock stats for `Saunshi` by searching the user in the `xbl` category.',
--- src/commands/r6/wp.ts
+++ src/commands/r6/wp.ts
@@ -1,12 +1,11 @@
 import { Command, CommandoClient } from 'discord.js-commando' // eslint-disable-line no-unused-vars
 import { getConfig } from './r6'
-import { constants } from '../../apis/rainbow'
 
 export default class R6WPWiki extends Command {
   constructor(client: CommandoClient) {
     super(client, getConfig('wp', {
       description: 'Displays weapon stats for the given weapon.',
-      details: `Specify the weapon to show by writing its name. Every weapon name or category can be written in lowercase and with \`-\` instead of spaces, e.g.: "Super Shorty" = "super-shorty".\nSupported weapon names are: ${constants.WEAPONS.map(wp => '`' + wp.name + '`').join(', ')}.\nSupported weapon categories are: ${Object.values(constants.WEAPONTYPES).map(wt => `\`${wt}\``).join(', ')}.`,
+      details: 'Specify the weapon to show by writing its name. Every weapon name or category can be written in lowercase and with `-` instead of spaces, e.g.: "Super Shorty" = "super-shorty".\nYou can find codes for supported weapons and weapon types here: <https://github.com/EndBug/game-tracker/blob/master/doc/r6/R6_NAMES.md#weapon-categories>',
       examples: {
         'camrs Snake_Nade': 'Displays CAMRS stats for `Snake_Nade` by searching the user in the `uplay` category.',
         'mk1-9mm Saunshi xbl': 'Displays MK1 9mm stats for `Saunshi` by searching the user in the `xbl` category.',
