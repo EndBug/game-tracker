b02609a648f77728a2b1e5cadfb439c0d3b95632
Federico Grandi <fgrandi30@gmail.com>
2020-03-03 16:53:16 +0100

    Trigger lint

--- src/core/app.ts
+++ src/core/app.ts
@@ -11,7 +11,7 @@ import * as fs from 'fs'
 import { API } from '../utils/utils' // eslint-disable-line no-unused-vars
 import * as stats_poster from '../utils/stats_poster'
 
-const { TOKEN } = process.env
+const { TOKEN } = process.env;
 
 export let client: Commando.CommandoClient
 export let homeguild: Discord.Guild
