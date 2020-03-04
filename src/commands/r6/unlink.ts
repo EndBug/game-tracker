299bf303197880401d0431c1d7f21fe7e4832925
Federico Grandi <fgrandi30@gmail.com>
2020-03-04 21:26:49 +0100

    Update help pages

--- src/commands/r6/unlink.ts
+++ src/commands/r6/unlink.ts
@@ -5,7 +5,6 @@ export default class R6UnlinkWiki extends Command {
   constructor(client: CommandoClient) {
     super(client, getConfig('unlink', {
       description: 'Deletes your account from the bot\'s database.',
-      details: 'You just need to enter your username; if you are playing on Xbox or PlayStation, add the platform after it (the default one is `uplay`)',
       examples: {
         '': 'Unlinks your account.'
       },
--- src/commands/r6/wp.ts
+++ src/commands/r6/wp.ts
@@ -4,13 +4,13 @@ import { getConfig } from './r6'
 export default class R6WPWiki extends Command {
   constructor(client: CommandoClient) {
     super(client, getConfig('wp', {
-      description: 'Displays weapon stats for the given weapon.',
+      description: 'Displays weapon stats for the given weapon or weapon category.',
       details: 'Specify the weapon to show by writing its name. Every weapon name or category can be written in lowercase and with `-` instead of spaces, e.g.: "Super Shorty" = "super-shorty".\nYou can find codes for supported weapons and weapon types here: <https://github.com/EndBug/game-tracker/blob/master/doc/r6/R6_NAMES.md#weapon-categories>',
       examples: {
         'camrs Snake_Nade': 'Displays CAMRS stats for `Snake_Nade` by searching the user in the `uplay` category.',
-        'mk1-9mm Saunshi xbl': 'Displays MK1 9mm stats for `Saunshi` by searching the user in the `xbl` category.',
+        'assault Saunshi xbl': 'Displays assault weapons stats for `Saunshi` by searching the user in the `xbl` category.',
         'L85A2 @YoMama': 'Displays L85A2 stats for the mentioned user, if they linked their account.',
-        'supernova': 'Displays your Supernova stats, if you linked your account.'
+        'pistol': 'Displays your pistol weapons stats, if you linked your account.'
       },
       extra: 'weaponName | weaponType'
     }))
