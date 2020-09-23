d2f1a74d00faf5a8f6998a5061f2c3ed07e85790
Federico Grandi <fgrandi30@gmail.com>
2020-09-23 09:53:02 +0200

    fix: properly initialize statcord

--- src/utils/statcord.ts
+++ src/utils/statcord.ts
@@ -1,7 +1,11 @@
 import { Client as StatClient } from 'statcord.js'
 import { client } from '../core/app'
 
-export const statcord = new StatClient({
-  client,
-  key: process.env.STATCORD_TOKEN
-})
+export let statcord: StatClient
+
+export const init = () => {
+  statcord = new StatClient({
+    client,
+    key: process.env.STATCORD_TOKEN
+  })
+}
